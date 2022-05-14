9ea932ed0fae252cb8b535354eb72993e2eed42c
LF56 <leah.fox7@gmail.com>
2022-05-13 20:32:44 -0400

    deploying to heroku

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 
@@ -21,20 +23,24 @@ const SingleThought = (props) => {
 
   return (
     <div>
-    <div className="card mb-3">
-      <p className="card-header">
-        <span style={{ fontWeight: 700 }} className="text-light">
-          {thought.username}
-        </span>{' '}
-        thought on {thought.createdAt}
-      </p>
-      <div className="card-body">
-        <p>{thought.thoughtText}</p>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {thought.username}
+          </span>{' '}
+          thought on {thought.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{thought.thoughtText}</p>
+        </div>
       </div>
+
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
-  
-    {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-  </div>
   );
 };
 
